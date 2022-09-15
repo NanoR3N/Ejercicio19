69275d680b4ad641983b6b3d45801b4f3a43ba1e
NANOR3N <NANO4REN@GMAIL.COM>
2022-09-15 00:41:44 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ejercicio19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero Entero");
+            int unNumero = int.Parse(Console.ReadLine());
+            if (unNumero < 0) Console.WriteLine("El numero ingresado no es un ENTERO");
+            else if (unNumero % 2 == 0) Console.WriteLine("El numero ingresado es Par");
+            else Console.WriteLine("El numero ingresado es Impar");
+            
+        }
+    }
+}
